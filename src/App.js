47ccd17a2a2521848dcac8e7176b619c20561c7a
pksatyam777 <pksatyam777@gmail.com>
2024-10-27 14:45:45 +0530

    fin

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
 
   return (
     <div>
-      <Router basename='/newsapp-sameer'>
+      <Router>
         <NavBar />
         <LoadingBar
           color='#FFFFFF'
@@ -28,7 +28,6 @@ const App = () => {
           height={3}
         />
         <Routes>
-          <Route path="*" element={<Navigate to="/" />} />
           <Route path="/" element={<News setProgress={setProgress} key="general" pageSize={pageSize} country="us" category="general" />} />
           <Route path="/business" element={<News setProgress={setProgress} key="business" pageSize={pageSize} country="us" category="business" />} />
           <Route path="/entertainment" element={<News setProgress={setProgress} key="entertainment" pageSize={pageSize} country="us" category="entertainment" />} />
@@ -37,6 +36,7 @@ const App = () => {
           <Route path="/science" element={<News setProgress={setProgress} key="science" pageSize={pageSize} country="us" category="science" />} />
           <Route path="/sports" element={<News setProgress={setProgress} key="sports" pageSize={pageSize} country="us" category="sports" />} />
           <Route path="/technology" element={<News setProgress={setProgress} key="technology" pageSize={pageSize} country="us" category="technology" />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </div>
