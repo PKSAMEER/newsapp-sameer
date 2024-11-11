255c057b00ba6d06210cb20d953d081de444998c
pksameer777 <pksameer777@gmail.com>
2024-11-12 00:59:01 +0530

    f

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import News from './components/News';
 import LoadingBar from 'react-top-loading-bar'
 
 import {
-  HashRouter as Router,
+  BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
@@ -20,7 +20,7 @@ const App = () => {
 
   return (
     <div>
-      <Router basename='{process.env.PUBLIC_URL}'>
+      <Router basename='/'>
         <NavBar />
         <LoadingBar
           color='#FFFFFF'
