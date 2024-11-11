b76b7a9f696068d46e89953541f64af6c272a2dc
pksameer777 <pksameer777@gmail.com>
2024-11-12 00:18:17 +0530

    f

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
 
   return (
     <div>
-      <Router basename='/'>
+      <Router basename='{process.env.PUBLIC_URL}'>
         <NavBar />
         <LoadingBar
           color='#FFFFFF'
